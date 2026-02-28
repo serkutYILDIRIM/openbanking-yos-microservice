b0876a3c185cfa5c810a8e5c9797c1854edfdb41
serkutYILDIRIM <serkutyldrm@gmail.com>
2026-02-28 23:09:44 +0300

    Initial commit

---
+++ src/main/java/com/openbanking/yos/OpenbankingYosMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.openbanking.yos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenbankingYosMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenbankingYosMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/openbanking/yos/OpenbankingYosMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openbanking.yos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenbankingYosMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
