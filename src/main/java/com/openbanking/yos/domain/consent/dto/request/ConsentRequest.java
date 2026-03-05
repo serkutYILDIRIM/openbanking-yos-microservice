897e3938c0c685248b85b2df7c947ec3357899a5
serkutYILDIRIM <serkutyldrm@gmail.com>
2026-03-05 16:41:18 +0300

    Add ConsentRequest and ConsentResponse DTOs with validation annotations

--- src/main/java/com/openbanking/yos/domain/consent/dto/request/ConsentRequest.java
+++ src/main/java/com/openbanking/yos/domain/consent/dto/request/ConsentRequest.java
@@ -1,5 +1,158 @@
 package com.openbanking.yos.domain.consent.dto.request;
 
-public class ConsentRequest {
-}
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ConsentRequest {
+
+    @NotNull
+    @Valid
+    @JsonProperty("katilimciBlg")
+    private KatilimciBilgisi katilimciBlg;
+
+    @NotNull
+    @Valid
+    @JsonProperty("gkd")
+    private Gkd gkd;
+
+    @NotNull
+    @Valid
+    @JsonProperty("odmBsltm")
+    private OdemeBaslatma odmBsltm;
+
+    // ----------------------------------------------------------------
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class KatilimciBilgisi {
+
+        @NotBlank
+        @JsonProperty("hhsKod")
+        private String hhsKod;
+
+        @NotBlank
+        @JsonProperty("yosKod")
+        private String yosKod;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Gkd {
+
+        @JsonProperty("yetYntm")
+        private String yetYntm;
+
+        @JsonProperty("yonAdr")
+        private String yonAdr;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Kimlik {
+
+        @JsonProperty("kmlkTur")
+        private String kmlkTur;
+
+        @JsonProperty("kmlkVrs")
+        private String kmlkVrs;
+
+        @NotBlank
+        @JsonProperty("ohkTur")
+        private String ohkTur;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Tutar {
+
+        @NotBlank
+        @JsonProperty("prBrm")
+        private String prBrm;
+
+        @NotBlank
+        @Pattern(regexp = "^\\d{1,18}$|^\\d{1,18}\\.\\d{1,5}$")
+        @JsonProperty("ttr")
+        private String ttr;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Hesap {
+
+        @JsonProperty("unv")
+        private String unv;
+
+        @JsonProperty("hspNo")
+        private String hspNo;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class OdemeAyrintilari {
+
+        @NotBlank
+        @JsonProperty("odmKynk")
+        private String odmKynk;
+
+        @NotBlank
+        @JsonProperty("odmAmc")
+        private String odmAmc;
+
+        @JsonProperty("refBlg")
+        private String refBlg;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class OdemeBaslatma {
+
+        @NotNull
+        @Valid
+        @JsonProperty("kmlk")
+        private Kimlik kmlk;
+
+        @NotNull
+        @Valid
+        @JsonProperty("islTtr")
+        private Tutar islTtr;
+
+        @Valid
+        @JsonProperty("gon")
+        private Hesap gon;
+
+        @NotNull
+        @Valid
+        @JsonProperty("alc")
+        private Hesap alc;
+
+        @NotNull
+        @Valid
+        @JsonProperty("odmAyr")
+        private OdemeAyrintilari odmAyr;
+    }
+}
--- src/main/java/com/openbanking/yos/domain/consent/dto/response/ConsentResponse.java
+++ src/main/java/com/openbanking/yos/domain/consent/dto/response/ConsentResponse.java
@@ -1,5 +1,165 @@
 package com.openbanking.yos.domain.consent.dto.response;
 
-public class ConsentResponse {
-}
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.openbanking.yos.common.enums.ConsentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ConsentResponse {
+
+    @JsonProperty("rzBlg")
+    private RizaBilgileri rzBlg;
+
+    @JsonProperty("katilimciBlg")
+    private KatilimciBilgisi katilimciBlg;
+
+    @JsonProperty("gkd")
+    private Gkd gkd;
+
+    @JsonProperty("odmBsltm")
+    private OdemeBaslatma odmBsltm;
+
+    // ----------------------------------------------------------------
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class RizaBilgileri {
+
+        @JsonProperty("rizaNo")
+        private String rizaNo;
+
+        @JsonProperty("olusZmn")
+        private LocalDateTime olusZmn;
+
+        @JsonProperty("gnclZmn")
+        private LocalDateTime gnclZmn;
+
+        @JsonProperty("rizaDrm")
+        private ConsentStatus rizaDrm;
+
+        @JsonProperty("rizaIptDtyKod")
+        private String rizaIptDtyKod;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class KatilimciBilgisi {
+
+        @JsonProperty("hhsKod")
+        private String hhsKod;
+
+        @JsonProperty("yosKod")
+        private String yosKod;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Gkd {
+
+        @JsonProperty("yetYntm")
+        private String yetYntm;
+
+        @JsonProperty("yonAdr")
+        private String yonAdr;
+
+        @JsonProperty("hhsYonAdr")
+        private String hhsYonAdr;
+
+        @JsonProperty("yetTmmZmn")
+        private LocalDateTime yetTmmZmn;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Kimlik {
+
+        @JsonProperty("kmlkTur")
+        private String kmlkTur;
+
+        @JsonProperty("kmlkVrs")
+        private String kmlkVrs;
+
+        @JsonProperty("ohkTur")
+        private String ohkTur;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Tutar {
+
+        @JsonProperty("prBrm")
+        private String prBrm;
+
+        @JsonProperty("ttr")
+        private String ttr;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Hesap {
+
+        @JsonProperty("unv")
+        private String unv;
+
+        @JsonProperty("hspNo")
+        private String hspNo;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class OdemeAyrintilari {
+
+        @JsonProperty("odmKynk")
+        private String odmKynk;
+
+        @JsonProperty("odmAmc")
+        private String odmAmc;
+
+        @JsonProperty("refBlg")
+        private String refBlg;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class OdemeBaslatma {
+
+        @JsonProperty("kmlk")
+        private Kimlik kmlk;
+
+        @JsonProperty("islTtr")
+        private Tutar islTtr;
+
+        @JsonProperty("gon")
+        private Hesap gon;
+
+        @JsonProperty("alc")
+        private Hesap alc;
+
+        @JsonProperty("odmAyr")
+        private OdemeAyrintilari odmAyr;
+    }
+}
