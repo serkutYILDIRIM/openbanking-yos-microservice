800e19190895b250ee71e629a5c8b886105738ba
serkutYILDIRIM <serkutyldrm@gmail.com>
2026-03-18 16:38:49 +0300

    createPayment method implemented in PaymentServiceImpl and add findByOdmEmriNo to PaymentRepository

--- src/main/java/com/openbanking/yos/domain/payment/service/PaymentService.java
+++ src/main/java/com/openbanking/yos/domain/payment/service/PaymentService.java
@@ -1,5 +1,10 @@
 package com.openbanking.yos.domain.payment.service;
 
+import com.openbanking.yos.domain.payment.dto.request.PaymentRequest;
+import com.openbanking.yos.domain.payment.dto.response.PaymentResponse;
+
 public interface PaymentService {
+
+    PaymentResponse createPayment(PaymentRequest request, String xRequestId, String xAspspCode, String xTppCode);
 }
 
