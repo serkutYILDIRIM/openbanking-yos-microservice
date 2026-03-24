560a89c33e066638609026f46a5e3829914af76c
serkutYILDIRIM <serkutyldrm@gmail.com>
2026-03-24 12:43:55 +0300

    Add account retrieval methods in AccountService with repository integration

--- src/main/java/com/openbanking/yos/domain/account/repository/AccountRepository.java
+++ src/main/java/com/openbanking/yos/domain/account/repository/AccountRepository.java
@@ -3,6 +3,13 @@ package com.openbanking.yos.domain.account.repository;
 import com.openbanking.yos.domain.account.entity.AccountEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<AccountEntity, Long> {
+
+    List<AccountEntity> findByRizaNo(String rizaNo);
+
+    Optional<AccountEntity> findByHspRef(String hspRef);
 }
 
