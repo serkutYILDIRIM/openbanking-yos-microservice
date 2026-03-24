1463b7d5d252d985c9f02dcabfabbbada1560c45
serkutYILDIRIM <serkutyldrm@gmail.com>
2026-03-24 12:37:47 +0300

    Lombok annotations added and structure to AccountListResponse and AccountResponse classes

--- src/main/java/com/openbanking/yos/domain/account/dto/response/AccountListResponse.java
+++ src/main/java/com/openbanking/yos/domain/account/dto/response/AccountListResponse.java
@@ -1,5 +1,20 @@
 package com.openbanking.yos.domain.account.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AccountListResponse {
+
+    @JsonProperty("hesaplar")
+    private List<AccountResponse> hesaplar;
 }
 
--- src/main/java/com/openbanking/yos/domain/account/dto/response/AccountResponse.java
+++ src/main/java/com/openbanking/yos/domain/account/dto/response/AccountResponse.java
@@ -1,5 +1,73 @@
 package com.openbanking.yos.domain.account.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AccountResponse {
+
+    @JsonProperty("rizaNo")
+    private String rizaNo;
+
+    @JsonProperty("hspTml")
+    private HesapTemel hspTml;
+
+    @JsonProperty("hspDty")
+    private HesapDetay hspDty;
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class HesapTemel {
+
+        @JsonProperty("hspRef")
+        private String hspRef;
+
+        @JsonProperty("hspNo")
+        private String hspNo;
+
+        @JsonProperty("hspShb")
+        private String hspShb;
+
+        @JsonProperty("subeAdi")
+        private String subeAdi;
+
+        @JsonProperty("kisaAd")
+        private String kisaAd;
+
+        @JsonProperty("prBrm")
+        private String prBrm;
+
+        @JsonProperty("hspTur")
+        private String hspTur;
+
+        @JsonProperty("hspTip")
+        private String hspTip;
+
+        @JsonProperty("hspUrunAdi")
+        private String hspUrunAdi;
+
+        @JsonProperty("hspDrm")
+        private String hspDrm;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class HesapDetay {
+
+        @JsonProperty("hspAclsTrh")
+        private LocalDateTime hspAclsTrh;
+    }
 }
 
