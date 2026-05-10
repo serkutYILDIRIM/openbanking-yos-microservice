e1cc27c4a099092a290a8df0ce7cdd7f00b547c0
serkutYILDIRIM <serkutyldrm@gmail.com>
2026-05-10 23:12:35 +0300

    Add unit tests for CardRequest to validate instance creation and properties

---
+++ src/test/java/com/openbanking/yos/domain/card/dto/request/CardRequestTest.java
@@ -0,0 +1,46 @@
+package com.openbanking.yos.domain.card.dto.request;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardRequestTest {
+
+    @Test
+    @DisplayName("CardRequest nesnesi başarıyla oluşturulmalı")
+    void shouldCreateCardRequest() {
+        CardRequest request = new CardRequest();
+
+        assertNotNull(request);
+    }
+
+    @Test
+    @DisplayName("CardRequest farklı nesneler birbirinden bağımsız olmalı")
+    void shouldCreateIndependentInstances() {
+        CardRequest request1 = new CardRequest();
+        CardRequest request2 = new CardRequest();
+
+        assertNotNull(request1);
+        assertNotNull(request2);
+        assertNotSame(request1, request2);
+    }
+
+    @Test
+    @DisplayName("CardRequest doğru pakette tanımlanmış olmalı")
+    void shouldBeInCorrectPackage() {
+        assertEquals("com.openbanking.yos.domain.card.dto.request", CardRequest.class.getPackageName());
+    }
+
+    @Test
+    @DisplayName("CardRequest public sınıf olmalı")
+    void shouldBePublicClass() {
+        assertTrue(java.lang.reflect.Modifier.isPublic(CardRequest.class.getModifiers()));
+    }
+
+    @Test
+    @DisplayName("CardRequest varsayılan constructor'a sahip olmalı")
+    void shouldHaveDefaultConstructor() throws NoSuchMethodException {
+        assertNotNull(CardRequest.class.getDeclaredConstructor());
+    }
+}
