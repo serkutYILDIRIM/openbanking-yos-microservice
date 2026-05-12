1cafcc4c8d25c9f4335a5373e540cdbd6c4423b0
Serkut Yıldırım <71320243+serkutYILDIRIM@users.noreply.github.com>
2026-05-12 21:53:49 +0300

    Refactor imports in CardEntityTest.java

--- src/test/java/com/openbanking/yos/domain/card/entity/CardEntityTest.java
+++ src/test/java/com/openbanking/yos/domain/card/entity/CardEntityTest.java
@@ -1,9 +1,7 @@
 package com.openbanking.yos.domain.card.entity;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.time.LocalDateTime;
+import org.junit.jupiter.api.Test;import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
