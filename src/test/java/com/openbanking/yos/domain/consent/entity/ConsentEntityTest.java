04a60fabf6a1aace37341afc3ab16414a0b6069c
Serkut Yıldırım <71320243+serkutYILDIRIM@users.noreply.github.com>
2026-05-14 22:39:06 +0300

    Update ConsentEntityTest.java

--- src/test/java/com/openbanking/yos/domain/consent/entity/ConsentEntityTest.java
+++ src/test/java/com/openbanking/yos/domain/consent/entity/ConsentEntityTest.java
@@ -5,7 +5,6 @@ import com.openbanking.yos.common.enums.GkdType;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class ConsentEntityTest {
